d844ca5aa526a4d49b4b159e4540c7c7868df829
Abdullah Moursi <moursiabdullah@gmail.com>
2021-12-29 20:49:01 +0200

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer, Auth } from "./components";
+
+const cookies = new Cookies();
+
+const apiKey = "pq6sdnmgzsvk";
+const authToken = cookies.get("token");
+
+const client = StreamChat.getInstance(apiKey);
+
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get("userId"),
+      name: cookies.get("username"),
+      fullname: cookies.get("fullname"),
+      image: cookies.get("avatarURL"),
+      hashedPassword: cookies.get("hashedPassword"),
+      phoneNumber: cookies.get("phoneNumber"),
+    },
+    authToken
+  );
+}
+
+const App = () => {
+  if (!authToken) return <Auth />;
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
