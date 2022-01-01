15c22682ea9611fa770aeb0602f50afebc06a0ab
Abdullah Moursi <moursiabdullah@gmail.com>
2022-01-01 18:16:01 +0200

    edit

--- client/src/components/ChannelInner.jsx
+++ client/src/components/ChannelInner.jsx
@@ -85,6 +85,9 @@ const TeamChannelHeader = ({ setIsEditing }) => {
       );
     }
 
+
+    
+
     return (
       <div className="team-channel-header__channel-wrapper">
         <p className="team-channel-header__name"># {channel.data.name}</p>
